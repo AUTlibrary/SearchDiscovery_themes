ea6b51e22c10632e3dbdfb968b3cca3b1f387733
cmurdoch <craig.murdoch@aut.ac.nz>
2018-12-03 15:14:16 +1300

    Update sandal/theme.config.php

--- sandal/theme.config.php
+++ sandal/theme.config.php
@@ -3,5 +3,9 @@ return [
     'extends' => 'bootstrap3',
     'css' => [
         'https://fonts.googleapis.com/css?family=Source+Sans+Pro',
-	]
+	],
+	"less" => array(
+      "active" => true/false,
+      "compiled.less"
+),
 ];	
